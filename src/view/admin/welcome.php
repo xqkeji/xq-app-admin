a4cecc554e3e5c368a2ce5ec26818d43d53cb774
xqkeji <xqkeji@foxmail.com>
2023-08-06 18:07:47 +0800

    更新系统介绍

--- src/view/admin/welcome.php
+++ src/view/admin/welcome.php
@@ -7,7 +7,7 @@ $this->setMainView("admin");
   </div>
   <div class="card-body">
     <p class="card-text">
-    <?=xq_p('xq-app-intro','&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;新齐通用后台管理系统是以新齐通用后台页面系统和新齐开发平台为基础开发的一套通用后台管理系统，可以帮助用户快速完成系统的开发。')?>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?=xq_p('xq-app-intro','新齐通用后台管理系统是以新齐通用后台页面系统和新齐开发平台为基础开发的一套通用后台管理系统，可以帮助用户快速完成系统的开发。')?>
     </p>
           
   </div>
