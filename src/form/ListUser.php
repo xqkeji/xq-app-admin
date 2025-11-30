770dc7a9a0eb1575367254675ee07d2bc1c5bb0f
xqkeji <xqkeji@foxmail.com>
2025-11-30 10:38:18 +0800

    自适应页面修复

--- src/form/ListUser.php
+++ src/form/ListUser.php
@@ -5,12 +5,12 @@ return [
 	'table'=>[],
 	[
 		'ListId',
-		['text'=>'用户名','name'=>'username'],
-		['text'=>'姓名','name'=>'fullname'],
+		['text'=>'用户名','name'=>'username','attr_style'=>'min-width:120px;',],
+		['text'=>'姓名','name'=>'fullname','attr_style'=>'min-width:120px;',],
 		'ListSwitch',
-		['text'=>'登录次数','name'=>'login_counts'],
+		['text'=>'登录次数','name'=>'login_counts','attr_style'=>'min-width:120px;',],
 		'ListLoginTime',
-		['text'=>'登录IP','name'=>'login_ip','attr_style'=>'width:120px;',],
+		['text'=>'登录IP','name'=>'login_ip','attr_style'=>'min-width:120px;',],
 		'ListEditDelete',
 	],
 	
--- src/form/ListUserRole.php
+++ src/form/ListUserRole.php
@@ -3,8 +3,8 @@ return [
 	'list_form',
 	[
 		'ListId',
-		['text'=>'角色名','name'=>'rolename'],
-		['text'=>'描述','name'=>'desc','attr_width'=>'200px'],
+		['text'=>'角色名','name'=>'rolename','attr_style'=>'min-width:120px'],
+		['text'=>'描述','name'=>'desc','attr_style'=>'min-width:200px'],
 		'ListSwitch',
 		'ListCreateTime',
 		'ListUpdateTime',
